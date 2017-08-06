e2dcbc3c9eae446869759faaa61f685565209949
Krzysztof Magosa <krzysztof@magosa.pl>
2017-08-07 00:19:26 +0200

    xxx

--- init.py
+++
@@ -1,8 +0,0 @@
-def initialize(population_size, length, init_function):
-    if population_size % 2 == 1:
-        raise ValueError("Population size must be even number.")
-
-    return [
-        [init_function() for _ in range(length)]
-        for _ in range(population_size)
-    ]
